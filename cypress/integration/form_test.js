d75d00e136afbc3d7bf0036aca749eeb2b29c737
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-23 13:18:28 -0700

    made a test that submits a user

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -6,5 +6,25 @@ describe('User Form', () => {
     it('input a username', () => {
         cy.get('input[name="username"]')
         .type('Nick')
+        .should('have.value', 'Nick')
+    })
+    it('input a email', () => {
+        cy.get('input[name="email"]')
+        .type('nick@users.com')
+        .should('have.value', 'nick@users.com')
+    })
+    it('input a password', () => {
+        cy.get('input[name="password"]')
+        .type('G00semoney')
+        .should('have.value', 'G00semoney')
+    })
+    it('check the terms', () => {
+        cy.get('input[name="terms"]')
+        .check()
+        .should('checked')
+    })
+    it('submit the user', () => {
+        cy.get('form button')
+        .click()
     })
 })
