f07677c22c3fe8f535a351b7524bb6e6778da89c
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-23 13:06:09 -0700

    able to type into input with cypress

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,7 +1,10 @@
 describe('User Form', () => {
-    it("navigates to the localhost", ()=> {
+    it('navigates to the localhost', ()=> {
         cy.visit('')
         cy.url().should('include', 'localhost')
     })
-
+    it('input a username', () => {
+        cy.get('input[name="username"]')
+        .type('Nick')
+    })
 })
